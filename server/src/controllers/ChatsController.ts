1d33db676daaba022495b81e7d9669f17b242655
soumyaarya <iec2023017@iiita.ac.in>
2026-07-15 18:35:06 +0530

    initial commit

---
+++ server/src/controllers/ChatsController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import prisma from "../config/db.config.js";
+
+class ChatsController {
+  //get everything to fetch list or collection of items
+  static async index(req: Request, res: Response) {
+    const { groupId } = req.params;
+    const chats = await prisma.chats.findMany({
+      where: {
+        group_id: groupId,
+      },
+    });
+    return res.json({ data: chats });
+  }
+}
+
+export default ChatsController;
